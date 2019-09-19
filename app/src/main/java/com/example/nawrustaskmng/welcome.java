6549bd064b95a7f3d3e4e92ce54997be9d4a40f5
Owner <alsalamcse>
2019-09-19 14:07:51 +0300

    nawrus1

--- app/src/main/java/com/example/nawrustaskmng/welcome.java
+++ app/src/main/java/com/example/nawrustaskmng/welcome.java
@@ -2,17 +2,34 @@ package com.example.nawrustaskmng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class welcome extends AppCompatActivity {
-    private ImageView img;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img=(ImageView)findViewById(R.id.img);
+
+        Thread th=new Thread()
+        {
+            public void run()
+            {
+                try {
+                    sleep(3*1000);
+                    Intent i=new Intent(getApplication(),SignIn1.class);
+                    startActivity(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        th.start();
+
     }
 }
