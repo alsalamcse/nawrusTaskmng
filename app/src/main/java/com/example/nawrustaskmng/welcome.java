d250dc40c09d5544b0371b0db9061902ae6ac9a7
Owner <alsalamcse>
2019-10-08 12:23:08 +0300

    nawrus1

--- app/src/main/java/com/example/nawrustaskmng/welcome.java
+++ app/src/main/java/com/example/nawrustaskmng/welcome.java
@@ -6,23 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class welcome extends AppCompatActivity {
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+    protected void onResume() {
+        super.onResume();
         Thread th=new Thread()
         {
             public void run()
             {
                 try {
                     sleep(3*1000);
-                    Intent i=new Intent(getApplication(),SignIn1.class);
-                    startActivity(i);
+                    FirebaseAuth auth=FirebaseAuth.getInstance();
+                    if(auth.getCurrentUser()==null || auth.getCurrentUser().getEmail()==null){
+                        Intent i=new Intent(getApplication(),SignIn1.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else{
+                        Intent i=new Intent(getApplication(),MainTasksActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -30,6 +39,15 @@ public class welcome extends AppCompatActivity {
 
         };
         th.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
 
     }
 }
