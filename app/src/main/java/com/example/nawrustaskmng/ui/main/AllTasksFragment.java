8692e33e54edab92786882fc8ff998f3d072d167
Owner <alsalamcse>
2019-10-27 12:20:52 +0200

    nawrus1

--- app/src/main/java/com/example/nawrustaskmng/ui/main/AllTasksFragment.java
+++ app/src/main/java/com/example/nawrustaskmng/ui/main/AllTasksFragment.java
@@ -3,16 +3,23 @@ package com.example.nawrustaskmng.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.nawrustaskmng.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import data.Mytask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,7 +44,21 @@ public class AllTasksFragment extends Fragment {
         FirebaseAuth auth=FirebaseAuth.getInstance();//to get current UID
         String uid = auth.getUid();
         DatabaseReference reference = database.getReference();
-        reference
+        reference.child("tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot d:dataSnapshot.getChildren()) {
+                    Mytask t=d.getValue(Mytask.class);
+                    Log.d("MYTASK",t.toString());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
