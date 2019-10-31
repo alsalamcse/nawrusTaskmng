90e8b6a261aec8d6c03be4546babd1ee7aed8de0
Owner <alsalamcse>
2019-10-31 10:49:23 +0200

    nawrus1

--- app/src/main/java/com/example/nawrustaskmng/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/nawrustaskmng/ui/main/SectionsPagerAdapter.java
@@ -19,17 +19,36 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @StringRes
     private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
     private final Context mContext;
+    private AllTasksFragment allTasksFragment;
+    private HistoryFragment historyFragment;
+    private RemovedFragment removedFragment;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
         mContext = context;
+        allTasksFragment=new AllTasksFragment();
+        historyFragment=new HistoryFragment();
+        removedFragment=new RemovedFragment();
+
     }
 
     @Override
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+
+        if(position==0)
+            return allTasksFragment;
+
+        if(position==1)
+
+            return historyFragment;
+
+        if(position==2)
+
+            return removedFragment;
+        return null;
+
     }
 
     @Nullable
