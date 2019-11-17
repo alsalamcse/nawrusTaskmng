762227524af6cfa43b0254718c5b5db25433ece3
Owner <alsalamcse>
2019-11-17 10:47:45 +0200

    nawrus1

--- app/src/main/java/data/TasksAdapter.java
+++ app/src/main/java/data/TasksAdapter.java
@@ -10,6 +10,7 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -53,9 +54,19 @@ public class TasksAdapter extends ArrayAdapter<Mytask> {
                     FireBaseUtils.getReference().child(mytask.getKey()).removeValue(new DatabaseReference.CompletionListener() {
                         @Override
                         public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            if(databaseError==null)
+                            {
+                                Toast.makeText(getContext(),"deleted",Toast.LENGTH_SHORT).show();
+
+
+                            }
+                            else {
+                                Toast.makeText(getContext(),"not deleted"+databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+
+                            }
 
                         }
-                    })
+                    });
 
                 }
             }
